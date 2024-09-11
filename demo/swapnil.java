64e894b0677238da30c45c194968b0a302adbcec
pratiklanke <pratiklanke883@gmail.com>
2024-09-11 17:06:12 +0530

    new commit

--- demo/swapnil.java
+++ demo/swapnil.java
@@ -2,5 +2,6 @@ public class swapnil {
     public static void main(String[] args) {
         System.out.println("This is my first commit 21st Aug 2024");
         System.out.println("Pratik Lanke - 10 sept");
+        System.out.println("Pratik Lanke - 11 sept");
     }
 }
